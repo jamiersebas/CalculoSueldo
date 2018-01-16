2ef5782e2486313b01a7199fe3feff892e2be1c7
jamiersebas <35357520+jamiersebas@users.noreply.github.com>
2018-01-15 23:46:27 -0600

    Primer commit

---
+++ CalculoSueldo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoSueldo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horasTrabajadas;
+            float costoHora;
+            float sueldo;
+            string linea;
+
+            Console.Write("Ingrese Horas trabajadas por el operario:");
+            linea = Console.ReadLine();
+            horasTrabajadas = int.Parse(linea);
+
+            Console.Write("Ingrese el pago por hora:");
+            linea = Console.ReadLine();
+            costoHora = float.Parse(linea);
+
+            sueldo = horasTrabajadas * costoHora;
+
+            Console.Write("El sueldo total del operario es:");
+            Console.Write(sueldo);
+            Console.ReadKey();
+        }
+    }
+}
